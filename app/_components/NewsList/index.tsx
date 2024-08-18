14466a15b06c96dfd32c89bd724c7487b56370f5
Hietan <hietan622@gmail.com>
2024-08-18 16:49:07 +0900

    5章まで完了

--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import styles from "./index.module.css";
 import Category from "../Category";
@@ -13,14 +14,23 @@ export default function NewsList({ news }: Props) {
 		<ul>
 			{news.map((article) => (
 				<li key={article.id} className={styles.list}>
-					<div className={styles.link}>
-						<Image
-							className={styles.image}
-							src="/no-image.png"
-							alt="No Image"
-							width={1200}
-							height={630}
-						/>
+					<Link href={`/news/${article.id}`} className={styles.link}>
+						{article.thumbnail ? (
+							<Image
+								src={article.thumbnail.url}
+								alt=""
+								className={styles.image}
+								width={article.thumbnail.width}
+								height={article.thumbnail.height}
+							/>
+						) : (
+							<Image
+								src="/no-image.png"
+								alt="No Image"
+								width={1200}
+								height={630}
+							/>
+						)}
 						<dl className={styles.content}>
 							<dt className={styles.tilte}>{article.title}</dt>
 							<dd className={styles.meta}>
@@ -28,7 +38,7 @@ export default function NewsList({ news }: Props) {
 								<Date date={article.publishedAt ?? article.createdAt} />
 							</dd>
 						</dl>
-					</div>
+					</Link>
 				</li>
 			))}
 		</ul>
